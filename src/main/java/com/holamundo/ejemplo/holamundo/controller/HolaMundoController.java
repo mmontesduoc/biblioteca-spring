9a048138dfcf4c6025a82f9f5d65bd2e1dce1f4e
mmontesduoc <marc.montes@duocuc.cl>
2025-04-26 10:56:17 -0400

    segundo commit

--- src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
@@ -3,9 +3,9 @@ package com.holamundo.ejemplo.holamundo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//control
 
-
-@RestController
+@RestController 
 public class HolaMundoController {
     @GetMapping("/hola")
     public String holaMundo () {
